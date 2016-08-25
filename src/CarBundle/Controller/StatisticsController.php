d2638fe6af849c0ea098f96ab180c5a70e53efe7
wloczynutka <wloczynutka@gmail.com>
2016-08-25 12:59:44 +0200

    distance in moth chart.

--- src/CarBundle/Controller/StatisticsController.php
+++ src/CarBundle/Controller/StatisticsController.php
@@ -11,13 +11,17 @@ class StatisticsController extends Controller
     public function drawChartsAction(Request $request, $carId)
     {
         $car = $this->get('car_service')->loadCarById($carId);
-
+        /* @var $fueling \CarBundle\Entity\CarFueling */
         foreach ($car->getFuelings() as $fueling) {
             if($fueling->getCurrency() === $car->getDefaultCurrency()) {
                 $chartsData['fuelPriceChart'][] = [$fueling->getDateTime()->format('Y-m-d'), $fueling->getPricePerLiter()];
                 if($fueling->getAverageSpeed() != null) {
                     $chartsData['avgSpeedConsumption'][] = [$fueling->getAverageSpeed(), $fueling->getAverageConsumptionByComputer()];
                 }
+                if(!isset($chartsData['monthDistance'][$fueling->getDateTime()->format('Y-m')])){
+                    $chartsData['monthDistance'][$fueling->getDateTime()->format('Y-m')] = 0;
+                }
+                $chartsData['monthDistance'][$fueling->getDateTime()->format('Y-m')] =  $chartsData['monthDistance'][$fueling->getDateTime()->format('Y-m')] + $fueling->getTripDistance();
             }
         }
         d($chartsData, $fueling);
